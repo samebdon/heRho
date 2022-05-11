b9a8435964ab7e59e51d4fe89504f1a01481dd92
Sam Ebdon <samebdon@gmail.com>
2022-05-11 16:05:34 +0100

    simulate to vcf

--- heRho/heRho_variant_simulator.py
+++ heRho/heRho_variant_simulator.py
@@ -23,6 +23,7 @@ import numpy as np
 import csv
 import concurrent.futures
 import datetime
+import os
 
 def sim_replicates(seeds):
 	
@@ -45,26 +46,9 @@ def sim_replicates(seeds):
 			random_seed=mutation_seed)
 			### discrete_genome=False)
 
-		#list of variant positions
-		var_site_list = [var.site.position for var in mutated_ts.variants()]
-		
-		#list of arrays of each chromosomes genotype at each variant position
-		genotype_list = [var.genotypes for var in mutated_ts.variants()]
-		haplotype_dict = {}
-		i = 0
-		sample_count = 1
-		while sample_count <= (samples):
-			individual_genotype = [genotype[i]+genotype[i+1] for genotype in genotype_list]
-			individual_genotype = [0 if x==2 else x for x in individual_genotype]
-			individual_sites = [a*b for a,b in zip(individual_genotype, var_site_list)]
-			individual_sites = [i for i in individual_sites if i != 0]
-			haplotype_dict[sample_count] = individual_sites
-			i+=2
-			sample_count += 1
-
 		print("Replicate seed " + str(ancestry_seed) + "/" + str(mutation_seed) + " completed.")
 		
-		return haplotype_dict
+		return mutated_ts
 
 def sim_wrapper(seeds, threads):
 	
@@ -72,22 +56,18 @@ def sim_wrapper(seeds, threads):
 		results = executor.map(sim_replicates, seeds)
 		S = np.array(list(results), dtype=object)
 
-		with open("chromosome_"+str(chromosome_number)+".csv", "w") as file_h:
-			writer = csv.writer(file_h, delimiter='\t')
-			rep_n = 1
-			for replicate in S:
-				for sample, pos in replicate.items():
-					writer.writerow([str(sample), str(rep_n), pos])
-				rep_n+=1
-
+		for idx, replicate in enumerate(S):
+			with open(path+"chromosome_"+str(chromosome_number)+".replicate_"+str(idx)+".vcf", "w") as vcf_f_h:
+				replicate.write_vcf(vcf_f_h, contig_id=str(chromosome_number))		
 
 #global parameters
 rng=np.random.RandomState()
+
 num_replicates = 100
 seeds = rng.randint(1, 2**31, size=(num_replicates, 2))
-theta=0.0071
-mu=5.4e-9
-ne= theta/(4*mu)
+theta = 0.0071
+mu = 5.4e-9
+ne = theta/(4*mu)
 pop_size=ne
 samples=10
 
@@ -101,6 +81,9 @@ threads = int(args['--threads'])
 
 gbp = float(gene_conversion_rate)/(4*ne)
 rbp = float(crossover_rate)/(4*ne)
+
+path=os.path.join("./heRho_simulations/", "chromosome_"+chromosome_number+"/")
+os.makedirs(path, exist_ok=True)
 
 begin_time = datetime.datetime.now()
 sim_wrapper(seeds=seeds, threads=threads)
